862de9ee47afa3ce4856d58ab6d0494c87fcd5f4
PaoDuroMacMoney <mlopez.rodrigo@gmail.com>
2018-03-18 15:09:07 -0300

    Referência a outros arquivos

--- Project/db_config.py
+++ Project/db_config.py
@@ -1,36 +1,38 @@
 import os
 from pymongo import MongoClient
 
+class db_config:
 
-def get_db_client():
+    @staticmethod
+    def get_db_client():
 
-    client = None
+        client = None
 
-    if __is_hostname_reachable('db_container'):
-        client = MongoClient('mongodb://bolao_user:bolao_pass@db_container:27017/admin')
-    else:
-        # if dev environment
-        client = MongoClient()
-
-    return client
-
-
-def __is_hostname_reachable(hostname):
-    import subprocess
-
-    try:
-        if os.name == 'nt':
-            cmd = ['ping', hostname, '-n', '1']
+        if db_config.__is_hostname_reachable('db_container'):
+            client = MongoClient('mongodb://bolao_user:bolao_pass@db_container:27017/admin')
         else:
-            cmd = ['ping', '-c', '1', hostname]
+            # if dev environment
+            client = MongoClient()
 
-        with open(os.devnull, 'w') as DEVNULL:
-            response = subprocess.check_call(cmd,
-                 stdout=DEVNULL,  # suppress output
-                 stderr=DEVNULL
-            )
-        return True
-    except subprocess.CalledProcessError:
-        return False
+        return client
+
+    @staticmethod
+    def __is_hostname_reachable(hostname):
+        import subprocess
+
+        try:
+            if os.name == 'nt':
+                cmd = ['ping', hostname, '-n', '1']
+            else:
+                cmd = ['ping', '-c', '1', hostname]
+
+            with open(os.devnull, 'w') as DEVNULL:
+                response = subprocess.check_call(cmd,
+                     stdout=DEVNULL,  # suppress output
+                     stderr=DEVNULL
+                )
+            return True
+        except subprocess.CalledProcessError:
+            return False
 
 
