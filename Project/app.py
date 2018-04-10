6bd1e1282bb72b30f2f087dc3191a76043627d92
Unknown <guilhermecgs@gmail.com>
2018-04-09 22:39:05 -0300

    Iniciando testes

--- Project/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-server = Flask(__name__)
-
-@server.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There baby!</h1>"
-
-if __name__ == "__main__":
-    server.run(host='0.0.0.0')
