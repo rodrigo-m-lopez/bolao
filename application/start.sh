e61b20f9b91e3dadfd7c68eae891256afe33e12d
Unknown <guilhermecgs@gmail.com>
2018-06-16 09:55:44 -0300

    loop_sh

--- application/start.sh
+++ application/start.sh
@@ -9,14 +9,21 @@ echo _    Running Crawler...
 python GloboEsporteCrawler.py
 echo _    Running Crawler...OK!
 
-echo _    Starting Cron...
-crontab crawler_cron
-service cron start
-echo _    Starting Cron...OK!
+# echo _    Starting Cron...
+# crontab crawler_cron
+# service cron start
+# echo _    Starting Cron...OK!
+
+echo _    Starting Loop...
+nohup sh ./loop.sh &
+echo _    Starting Loop...OK!
 
 echo "Environment variables..."
 export
 echo "Environment variables...OK!"
+
+echo "All proccesses..."
+ps aux
 
 echo _Starting...OK!
 gunicorn -w 1 -b :8000 app:app
