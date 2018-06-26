f32f0dbd629f9cf53924fffb9e5c07b6a2b6f000
guilhermecgs <guilhermecgs@gmail.com>
2018-06-26 14:29:50 -0300

    Update bolao.secrets.env.bat

--- application/bolao.secrets.env.bat
+++ application/bolao.secrets.env.bat
@@ -1,5 +1,5 @@
-setx GOOGLE_OAUTH_CREDENTIAL_ID aaa
-setx GOOGLE_OAUTH_CREDENTIAL_SECRET bbb
+setx GOOGLE_OAUTH_CREDENTIAL_ID 760126712590-qaul287sj1bkqr46hubnmh9udvq3rrn6.apps.googleusercontent.com
+setx GOOGLE_OAUTH_CREDENTIAL_SECRET RK0abVTO0Q5CZliyiuB-nQxM
 
 echo Please reopen all proccess now to refresh...
 
